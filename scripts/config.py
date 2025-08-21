32e0c3416ea3a815b0141d8b845ec526b23fea7e
Fardad Maghsoudi Moud <fmaghsoudimoud@tudelft.nl>
2025-08-21 11:26:01 +0200

    More links are added

--- scripts/config.py
+++ scripts/config.py
@@ -6,22 +6,30 @@ WEB_URLS = [
     # main URL of the RDM TU Delft Library
     "https://www.tudelft.nl/en/library/data-management#c1634274"
     # data management plan
-    "https://www.tudelft.nl/en/library/data-management/data-management-plan"
+    "https://www.tudelft.nl/en/library/data-management/data-management-plan",
+    "https://www.tudelft.nl/library/data-management/trainingen/trainingen-voor-onderzoekers-en/research-data-management-101"
     # data management grant proposal
     "https://www.tudelft.nl/en/library/data-management/research-data-management/data-management-sections-for-grant-proposals"
     # about data stewardship
     "https://www.tudelft.nl/en/library/data-management/get-support-on-data-management/data-stewardship-about"
+    # research data management
+    "https://www.tudelft.nl/en/library/data-management/research-data-management/the-goal-of-research-data-management"
+    # TU Delft iRODS
+    "https://www.tudelft.nl/en/2023/library/easy-fast-accessible-and-secure-research-data-management", 
+    "https://hackmd.io/@fardadmaghsoudi/By5RxKF_h"
+    # security
+    "https://tud365.sharepoint.com/:u:/r/sites/SecurityPrivacyTUD/SitePages/en/Informatiebeveiliging-Nieuws.aspx?csf=1&web=1&e=q8hYWr",
+    "https://tud365.sharepoint.com/sites/SecurityPrivacyTUD/SitePages/en/Noodzakelijkheid-van-Dataclassificatie.aspx",
+    "https://tud365.sharepoint.com/sites/SecurityPrivacyTUD/SitePages/en/security-privacy-training.aspx",
+    # privacy
+    "https://tud365.sharepoint.com/:u:/r/sites/SecurityPrivacyTUD/SitePages/en/Privacy.aspx?csf=1&web=1&e=kGMFIQ",
+    # security and privacy awareness
+    "https://tud365.sharepoint.com/sites/SecurityPrivacyTUD/SitePages/en/Beleid-privacy.aspx"
+    # working safely
+    "https://tud365.sharepoint.com/sites/SecurityPrivacyTUD/SitePages/en/Awareness.aspx"
     # library
     "https://www.tudelft.nl/library",
     "https://www.tudelft.nl/en/about-tu-delft"
-    # research data management
-    "https://www.tudelft.nl/en/library/data-management/research-data-management/the-goal-of-research-data-management",
-    # security
-    "https://tud365.sharepoint.com/:u:/r/sites/SecurityPrivacyTUD/SitePages/en/Informatiebeveiliging-Nieuws.aspx?csf=1&web=1&e=q8hYWr",
-    # privacy
-    "https://tud365.sharepoint.com/:u:/r/sites/SecurityPrivacyTUD/SitePages/en/Privacy.aspx?csf=1&web=1&e=kGMFIQ",
-    # working safely
-    "https://tud365.sharepoint.com/sites/SecurityPrivacyTUD/SitePages/en/Awareness.aspx"
 ]
 MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.2"
 HF_TOKEN = os.environ.get("HF_TOKEN")
