d917bfd60b0b355c832cea7d2d1e30df4124ae49
Madalina Fron <omfron@rdmchatbot01.ceg.tudelft.nl>
2026-03-30 17:14:32 +0200

    ups

--- scripts/main.py
+++ scripts/main.py
@@ -239,7 +239,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app, 
         host=os.getenv("HOST","0.0.0.0"), 
-        port=os.getenv("PORT", 8000), 
+        port=int(os.getenv("PORT", 8000)), 
         proxy_headers=True, 
         forwarded_allow_ips=os.getenv("FORWARDED_ALLOW_IPS", "127.0.0.1"),
     )
